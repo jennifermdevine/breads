69f7d9c3a12a92b6976d8229d569e616dcba0d8e
Jennifer Devine <jennifermdevine88@gmail.com>
2023-06-22 21:15:08 -0700

    added title for index page

--- views/layout/default.jsx
+++ views/layout/default.jsx
@@ -2,17 +2,17 @@ const React = require('react')
 
 function Default(html) {
     return (
-    <html>
-        <head>
-        <title>Default</title>
-        </head>
-        <body>
-        <h1>HTML Rendered!</h1>
-        <div className="container">
-            {html.children}
-        </div>
-        </body>
-    </html>
+        <html lang="en">
+            <head>
+                <title>{html.title || 'Default'}</title>
+            </head>
+            <body>
+                <h1>HTML Rendered!</h1>
+                <div className="container">
+                    {html.children}
+                </div>
+            </body>
+        </html>
 )
 }
 
