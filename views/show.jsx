5ccecba996e460df537eb1991011dfd1f52108c1
Jennifer Devine <jennifermdevine88@gmail.com>
2023-06-27 20:47:35 -0700

    Edit and Update routes functional

--- views/show.jsx
+++ views/show.jsx
@@ -6,10 +6,7 @@ function Show ({bread, index}) {
     return(
         <Default>
             
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
-            <input type='submit' value="DELETE"/>
-            </form>
-
+            
             <h2>Show Page</h2>
             <h3>{bread.name}</h3>
             <p>
@@ -22,6 +19,13 @@ function Show ({bread, index}) {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
+
+            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <input type='submit' value="DELETE"/>
+            </form>
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
+
             <li><a href="/breads">Go home</a></li>
         </Default>
     )
