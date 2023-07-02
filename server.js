2d56a39a49b5ba0894a08280f57557146edcbf62
Jennifer Devine <jennifermdevine88@gmail.com>
2023-07-02 10:48:11 -0700

    installed & connected mongodb w/mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 
+const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 const methodOverride = require('method-override')
@@ -20,6 +21,11 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(methodOverride ('_method'))
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
 // ROUTES
 
