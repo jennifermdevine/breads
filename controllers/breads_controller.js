5f3ec241a3c700c1682e5b0a9a2b4603e83df3b8
Jennifer Devine <jennifermdevine88@gmail.com>
2023-07-06 19:17:47 -0700

    placed seed array into separate file & exported it

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,8 @@ const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/bread.js');
 
+const seedData = require('../seeds')
+
 //INDEX /breads/
 
 breads.get('/', (req, res) => {
@@ -66,7 +68,7 @@ breads.post ('/', (req, res) => {
 
 // DELETE
 
-breads.delete('/:indexArray', (req, res) => {
+breads.delete('/:id', (req, res) => {
     Bread.findByIdAndDelete(req.params.indexArray)
         .then(deletedBread => {
         res.status(303).redirect('/breads')
@@ -91,28 +93,7 @@ breads.put('/:id', (req, res) => {
 // SEED ROUTE
 
 breads.get('/data/seed', (req, res) => {
-    Bread.insertMany([
-        {
-            name: 'Rye',
-            hasGluten: true,
-            image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-        },
-        {
-            name: 'French',
-            hasGluten: true,
-            image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
-        },
-        {
-            name: 'Gluten Free',
-            hasGluten: false,
-            image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-        },
-        {
-            name: 'Pumpernickel',
-            hasGluten: true,
-            image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
-        }
-    ])
+    Bread.insertMany(seedData)
         .then(createdBreads => {
         res.redirect('/breads')
         })
